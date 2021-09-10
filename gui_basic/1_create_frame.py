b5b48da3bd311b0ca9633f98052b1964b431f7dd
shavit-es <71497140+shavit-es@users.noreply.github.com>
2021-09-10 22:08:35 +0900

    create frame

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("HH GUI")
+root.geometry("640x480") # 가로 * 세로
+# root.geometry("640x480+400+300") # 가로 * 세로 + x좌표, +y좌표
+root.resizable(False, False) #x(너비), y(높이) 값 각각 변경 불가
+
+
+root.mainloop()

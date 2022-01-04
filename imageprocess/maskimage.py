f054f5724309989cf6fe071ee85840bcd1c05173
shavit-es <71497140+shavit-es@users.noreply.github.com>
2022-01-04 11:07:45 +0900

    Update maskimage.py

--- imageprocess/maskimage.py
+++ imageprocess/maskimage.py
@@ -1,19 +1,19 @@
 import json
 from PIL import Image, ImageDraw
-
-file_path = "./part.json"
-with open(file_path, "r") as json_file:
-    json_data = json.load(json_file)
+with open("Serial Detection.json", 'r', encoding='UTF-8') as json_file:
+    json_data=json.load(json_file)
     for ele in json_data['data']:
         try:
-            imagei = Image.open('C:/Users/QA/Desktop/HH/prestudy/swissgoldbar/image/'+ele['fileName'])
+            imagei = Image.open(direction+'ele['fileName'])
             left, up, right, down = ele['regionLabel'][0]['x'], ele['regionLabel'][0]['y'], ele['regionLabel'][0]['x']+ele['regionLabel'][0]['width'], ele['regionLabel'][0]['y']+ele['regionLabel'][0]['height']
             cropped = imagei.crop((left, up, right, down))
             mask=Image.new("RGB",imagei.size, color="#000")
             mask.paste(cropped,(left,up))
-            mask.save("C:/Users/QA/Desktop/HH/prestudy/swissgoldbar/maskedimages/"+ele['fileName'])
+            mask.save(direction+ele['fileName'])
             print(ele['fileName']+' done')
-        except:
-            print('missing file : '+ ele['fileName'])
+        except FileNotFoundError as f:
+            print('missing file : '+ ele['fileName'], f)
             continue
+        except IndexError as e:
+            print("not used" + ele['fileName'], e)
 print('done')
